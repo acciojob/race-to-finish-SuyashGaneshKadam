37d364314ef614266de16b83fef140a632c52900
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-11-27 17:13:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+function createPromise(delay){
+	return new Promise((res, rej) =>{
+		setTimeout(() =>{
+			res("Promise resolved");
+		}, delay);
+	});
+}
+
+for(let i=0 ; i<5 ; i++){
+	promises.push(createPromise(Math.random() * (5000 - 1000) + 1000));
+}
+
+let output = document.getElementById("output");
+output.innerText = Promise.any(promises);
